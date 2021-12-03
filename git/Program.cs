0601bd674101e037609b13d1054244214019ad86
Jefferson Mourent <jmourent@ciputra.student.ac.id>
2021-12-03 10:32:41 +0700

    Menentukan bilangan prima atau bukan bilangan prima

--- git/Program.cs
+++ git/Program.cs
@@ -4,9 +4,26 @@ namespace git
 {
     class Program
     {
+        // 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
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int inputAngka = Convert.ToInt32(Console.ReadLine());
+            int faktor = 0;
+            for (int i=1; i<=inputAngka; i++)
+            {
+                if (inputAngka % i == 0)
+                {
+                    faktor++;
+                }
+            }
+            if (faktor == 2)
+            {
+                Console.WriteLine("Bilangan Prima");
+            }
+            else
+            {
+                Console.WriteLine("Bukan Bilangan Prima");
+            }
         }
     }
 }
